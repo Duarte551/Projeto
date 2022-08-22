1ea64c74bafe702eafd4d18e031a71a64e765bfd
Duarte551 <99897875+Duarte551@users.noreply.github.com>
2022-08-22 14:41:30 -0300

    Delete Atividade directory

--- Atividade/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-$login = $_SESSION['login'] ?? False;
-
-require_once __DIR__ . '/app/db/conexao.php';
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/utilities/telainicial.php';
-?>
